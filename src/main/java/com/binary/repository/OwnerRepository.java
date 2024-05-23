1a182bb20d7c027218aca965d8ea07bd29ea92a9
Natnael Bekele <natnaelbekele@Natnaels-Air.hsd1.va.comcast.net>
2024-05-23 13:17:08 -0400

    Added car show added

---
+++ src/main/java/com/binary/repository/OwnerRepository.java
@@ -0,0 +1,8 @@
+package com.binary.repository;
+
+import com.binary.Entity.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+}
